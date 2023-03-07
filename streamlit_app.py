e64b8573b69126a737b05b9e7a9973f44fd00f38
alexwu211 <alexwu211@139.com>
2023-03-07 21:29:13 +0800

    Update streamlit_app.py
    add Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & TRocket Smoothie')
-streamlit.text('Head-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & TRocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
