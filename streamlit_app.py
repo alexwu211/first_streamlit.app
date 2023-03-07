aa96a11fd4d3c90db2386bb8a85e536b968777f3
alexwu211 <alexwu211@139.com>
2023-03-07 21:24:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('Hello! this is first apps for streamlit')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & TRocket Smoothie')
+streamlit.text('Head-Boiled Free-Range Egg')
